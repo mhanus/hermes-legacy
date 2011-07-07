8422ddc7fed224f7cc5710c673785531e0da9e87
Milan Hanus <mhanus@kma.zcu.cz>
2011-07-07 11:01:33 +0200

    Unifying visualization in neutronics examples.

--- hermes2d/examples/neutronics/eigenvalue/1-group-hexagonal/diffusion/definitions.h
+++ hermes2d/examples/neutronics/eigenvalue/1-group-hexagonal/diffusion/definitions.h
@@ -16,34 +16,3 @@ class CustomWeakForm : public DefaultWeakFormSourceIteration
 
 void report_num_dof(const std::string& msg, const Hermes::vector<Space *> spaces);
                     
-class Views
-{
-  unsigned int n_unknowns, n_equations, n_groups;
-  bool display_meshes;
-  
-  ScalarView** sviews;
-  OrderView** oviews;
-  MeshView** mviews;
-  
-  static const std::string  base_title_flux;
-  static const std::string  base_title_order;
-  static const std::string  base_title_mesh;
-    
-  std::string itos(int t)
-  {
-    std::stringstream ss; ss << t;
-    return ss.str();
-  }
-  
-  public:
-    Views(unsigned int G, bool display_meshes = false);
-    ~Views();
-    
-    void show_meshes(Hermes::vector<Mesh*> meshes);
-    void show_solutions(Hermes::vector< Solution* > solutions);
-    void show_orders(Hermes::vector<Space*> spaces);
-    
-    void inspect_meshes(Hermes::vector<Mesh*> meshes);
-    void inspect_solutions(Hermes::vector< Solution* > solutions);
-    void inspect_orders(Hermes::vector<Space*> spaces);
-};
